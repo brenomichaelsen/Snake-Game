6e273f4d940d2d14a0a568b42b5ccac518a92e59
Breno Michaelsen <brenomichaelsen@gmail.com>
2023-02-14 14:24:20 -0300

    commit inicial

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.io.FileNotFoundException;
+
+public class fliperama{
+
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        System.out.println( "welcome to Snakinha game\n"+
+        
+                            "\nPlease choose an option:\n"+
+
+                            "\n1 - New Game"            +
+                            "\n2 - View Last games");
+    }
+}
